dee2d709287cd0c099441fd26990a1e633f29832
hritikb27 <hritikb90@gmail.com>
2021-10-19 14:01:58 +0530

    styled a bit

--- DOM.js
+++ DOM.js
@@ -1,12 +1,18 @@
+const mainDiv = document.querySelector('.main')
+
 function appendToDom(temp, desc){
+    if(document.querySelector('.content')){
+        document.querySelector('.content').remove();
+    }
     const createDiv = document.createElement('div');
+    createDiv.setAttribute('class', 'content')
     const weatherTemperature = document.createElement('p');
     const weatherDescription = document.createElement('p');
-    weatherTemperature.textContent = temp;
+    weatherTemperature.textContent = `${temp}°C`;
     weatherDescription.textContent = desc;
     createDiv.appendChild(weatherTemperature);
     createDiv.appendChild(weatherDescription);
-    document.body.appendChild(createDiv);
+    mainDiv.appendChild(createDiv);
 }
 
 export default appendToDom;
